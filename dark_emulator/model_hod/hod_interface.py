565928fb18ba41396c7b814685ef8f9729450c98
Takahiro Nishimichi <t243chi@gmail.com>
2021-08-30 23:26:31 +0900

    Bug fix: an update of the parameter "sat_dist_type" should now be in effect.

--- dark_emulator/model_hod/hod_interface.py
+++ dark_emulator/model_hod/hod_interface.py
@@ -189,6 +189,8 @@ class darkemu_x_hod(base_class):
         """
         # gparams includes galaxy-related parameters such as HOD, offcentering, incompleteness, and pi_max.
         if self.gparams != gparams:
+            if self.gparams and self.gparams["sat_dist_type"] != gparams["sat_dist_type"]:
+                self.p_hm_satdist_computed = False
             self.gparams = gparams
             self.HOD_computed = False
             self.ng_computed = False
