a6a199bbc646a42e592744f61c8ee255dab1450e
Takahiro Nishimichi <17330301+nishimichi@users.noreply.github.com>
2023-04-14 14:43:10 +0900

    version updated to 1.1.0

--- dark_emulator/__init__.py
+++ dark_emulator/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Takahiro Nishimichi, Hironao Miyatake'
-__version__ = '1.0.19'
+__version__ = '1.1.0'
 __url__ = 'https://dark-emulator.readthedocs.io'
 from . import darkemu
 from . import model_hod
